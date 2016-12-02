05c7fc406e4e1c35f4b08a68bd0fed612e941451
Богдан Рибак < >
2016-12-02 13:25:17 +0200

    Runtime permitions

--- app/src/main/java/com/example/bogda/geekhubandroidgrouplist/contacts/ContactsFragment.java
+++ app/src/main/java/com/example/bogda/geekhubandroidgrouplist/contacts/ContactsFragment.java
@@ -3,24 +3,30 @@ package com.example.bogda.geekhubandroidgrouplist.contacts;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Contacts;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.bogda.geekhubandroidgrouplist.R;
 import com.example.bogda.geekhubandroidgrouplist.data.Contact;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.jar.Manifest;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -31,6 +37,8 @@ import static android.app.Activity.RESULT_OK;
 public class ContactsFragment extends Fragment {
     ListView contactsListView = null;
     Button addButton = null;
+    public static final int PERMITION_READ_CONTACT_CODE = 1;
+    public static final int PERMITION_WRITE_CONTACT_CODE = 2;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -43,21 +51,21 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        ArrayList<Contact> contacts = new ArrayList<Contact>();
-        Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
-        while (phones.moveToNext()) {
-            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            contacts.add(new Contact(name,phoneNumber));
-        }
-        Collections.sort(contacts);
-        contactsListView.setAdapter(new ContactsAdapter(getContext(),contacts));
-
+        getContactsList();
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),AddDialog.class);
-                startActivityForResult(intent,1);
+                if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.WRITE_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(getActivity(), AddDialog.class);
+                    startActivityForResult(intent, 1);
+                }
+
+                else{
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),android.Manifest.permission.WRITE_CONTACTS)){
+                        Toast.makeText(getContext(),"Check permition in settings",Toast.LENGTH_LONG).show();
+                    }
+                    requestPermissions(new String[]{android.Manifest.permission.WRITE_CONTACTS}, PERMITION_WRITE_CONTACT_CODE);
+                }
             }
         });
     }
@@ -65,14 +73,15 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK){
+        if (resultCode == RESULT_OK) {
             addContact(data.getStringExtra("name"), data.getStringExtra("phone"));
         }
     }
+
     private void addContact(String name, String number) {
         ContentValues cv = new ContentValues();
         cv.put(Contacts.People.NAME, name);
-        Uri u =    getActivity().getContentResolver().insert(Contacts.People.CONTENT_URI, cv);
+        Uri u = getActivity().getContentResolver().insert(Contacts.People.CONTENT_URI, cv);
         Uri pathu = Uri.withAppendedPath(u, Contacts.People.Phones.CONTENT_DIRECTORY);
         cv.clear();
         cv.put(Contacts.People.NUMBER, number);
@@ -81,13 +90,53 @@ public class ContactsFragment extends Fragment {
 
         //update list
         ArrayList<Contact> contacts = new ArrayList<Contact>();
-        Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
         while (phones.moveToNext()) {
             String contactName = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            contacts.add(new Contact(contactName,phoneNumber));
+            contacts.add(new Contact(contactName, phoneNumber));
         }
         Collections.sort(contacts);
-        contactsListView.setAdapter(new ContactsAdapter(getContext(),contacts));
+        contactsListView.setAdapter(new ContactsAdapter(getContext(), contacts));
+    }
+
+    private void getContactsList() {
+        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            ArrayList<Contact> contacts = new ArrayList<Contact>();
+            Cursor phones = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while (phones.moveToNext()) {
+                String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contacts.add(new Contact(name, phoneNumber));
+            }
+            Collections.sort(contacts);
+            contactsListView.setAdapter(new ContactsAdapter(getContext(), contacts));
+        } else {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),android.Manifest.permission.READ_CONTACTS)){
+                Toast.makeText(getContext(),"Check permition in settings",Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[]{android.Manifest.permission.READ_CONTACTS}, PERMITION_READ_CONTACT_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMITION_READ_CONTACT_CODE:
+                if(grantResults.length==1) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        getContactsList();
+                    }
+                }
+                break;
+            case PERMITION_WRITE_CONTACT_CODE:
+                if(grantResults.length==1){
+                    if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                        Intent intent = new Intent(getActivity(), AddDialog.class);
+                        startActivityForResult(intent, 1);
+                    }
+                }
+        }
     }
 }
